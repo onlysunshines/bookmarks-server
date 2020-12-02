495fc1a5f219d25bbe76f9b4aa78f6abb1b7def3
Tomás Mier <68940061+onlysunshines@users.noreply.github.com>
2020-12-02 09:24:51 -0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,13 +1,5 @@
-const knex = require('knex')
 const app = require('./app')
-const { PORT, DB_URL } = require('./config')
-
-const db = knex({
-  client: 'pg',
-  connection: DB_URL,
-})
-
-app.set('db', db)
+const { PORT } = require('./config')
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
